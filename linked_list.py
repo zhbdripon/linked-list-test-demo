d1cbb1cea55f3a734f93e0b89893f539f8867b49
Ziaul Hoque Ripon <zhbdripon@gmail.com>
2021-05-29 00:17:12 +0600

    boilerplate

---
+++ linked_list.py
@@ -0,0 +1,50 @@
+class MyLinkedList(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        pass
+        
+
+    def get(self, index):
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        :type index: int
+        :rtype: int
+        """
+        pass
+
+    def addAtHead(self, val):
+        """
+        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        :type val: int
+        :rtype: None
+        """
+        pass
+
+    def addAtTail(self, val):
+        """
+        Append a node of value val to the last element of the linked list.
+        :type val: int
+        :rtype: None
+        """
+        pass
+
+    def addAtIndex(self, index, val):
+        """
+        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        :type index: int
+        :type val: int
+        :rtype: None
+        """
+        pass
+        
+
+    def deleteAtIndex(self, index):
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        :type index: int
+        :rtype: None
+        """
+        pass
