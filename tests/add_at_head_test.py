5e3841521b82c6b71167f1099020162c9e745409
Ziaul Hoque Ripon <zhbdripon@gmail.com>
2021-06-01 02:10:40 +0600

    test: update addAtHead tests

--- tests/add_at_head_test.py
+++ tests/add_at_head_test.py
@@ -1,6 +1,5 @@
 from linked_list import MyLinkedList
 from random import randint
-from random import seed
 import unittest
 
 class AddAtHeadTest(unittest.TestCase):
@@ -12,79 +11,68 @@ class AddAtHeadTest(unittest.TestCase):
             self.list.addAtHead(randint(1,1000))
 
     def test_add_at_head_empty_list(self):
-        for i in range(1,1001):
-            self.list.addAtHead(i)
-            self.assertEqual(i,self.list.get(0))
-            self.list = MyLinkedList()
+        self.list.addAtHead(42)
+        self.assertEqual(42,self.list.get(0))
 
     def test_add_at_head_in_a_row(self):
-        for i in range(1,1001):
-            self.list.addAtHead(i)
-            self.assertEqual(i,self.list.get(0))
+        self.list.addAtHead(42)
+        self.list.addAtHead(15)
+        self.assertEqual(42,self.list.get(1))
+        self.assertEqual(15,self.list.get(0))
 
     def test_add_at_head_after_add_at_tail(self):
-        for i in range(1,1001):
-            self.list.addAtTail(-i)
-            self.list.addAtHead(i)
-            self.assertEqual(i,self.list.get(0))
+        self.list.addAtTail(85)
+        self.list.addAtHead(45)
+        self.assertEqual(45,self.list.get(0))
 
     def test_add_at_head_after_get_operation(self):
         self.populate_list(5)
-        for i in range(1,1001):
-            self.list.get(0)
-            self.list.addAtHead(i)
-            self.assertEqual(i,self.list.get(0))
+        self.list.get(0)
+        self.list.addAtHead(15)
+        self.assertEqual(15,self.list.get(0))
 
     def test_add_at_head_after_add_at_index_0(self):
-        for i in range(1,1001):
-            self.list.addAtIndex(0,-i)
-            self.list.addAtHead(i)
-            self.assertEqual(i,self.list.get(0))
+        self.list.addAtIndex(0,45)
+        self.list.addAtHead(73)
+        self.assertEqual(73,self.list.get(0))
 
-    def test_add_at_head_after_add_at__last_index(self):
-        last_index = 0
-        for i in range(1,1001):
-            self.list.addAtIndex(last_index,-i)
-            self.list.addAtHead(i)
-            self.assertEqual(i,self.list.get(0))
-            last_index+=1
+    def test_add_at_head_after_add_at_last_index(self):
+        self.list.addAtTail(45)
+        self.list.addAtIndex(1,19)
+        self.list.addAtHead(54)
+        self.assertEqual(54,self.list.get(0))
+
 
     def test_add_at_head_after_add_at_random_index(self):
-        self.populate_list(1000)
-        for i in range(1,1001):
-            self.list.addAtIndex(randint(0,999),randint(0,999))
-            self.list.addAtHead(i)
-            self.assertEqual(i,self.list.get(0))
+        self.populate_list(10)
+        self.list.addAtIndex(7,28)
+        self.list.addAtHead(82)
+        self.assertEqual(82,self.list.get(0))
 
     def test_add_at_head_after_delete_at_index_0(self):
         self.populate_list(1000)
-        for i in range(1,1001):
-            self.list.deleteAtIndex(0)
-            self.list.addAtHead(i)
-            self.assertEqual(i,self.list.get(0))
+        self.list.deleteAtIndex(0)
+        self.list.addAtHead(37)
+        self.assertEqual(37,self.list.get(0))
 
     def test_add_at_head_after_delete_at_last_index(self):
         self.populate_list(1000)
-        for i in range(1,1001):
-            self.list.deleteAtIndex(999)
-            self.list.addAtHead(i)
-            self.assertEqual(i,self.list.get(0))
+        self.list.deleteAtIndex(999)
+        self.list.addAtHead(13)
+        self.assertEqual(13,self.list.get(0))
 
     def test_add_at_head_after_delete_at_random_index(self):
         self.populate_list(1000)
-        for i in range(1,1001):
-            self.list.deleteAtIndex(randint(0,999))
-            self.list.addAtHead(i)
-            self.assertEqual(i,self.list.get(0))
+        self.list.deleteAtIndex(125)
+        self.list.addAtHead(45)
+        self.assertEqual(45,self.list.get(0))
 
-    def test_add_at_head_after_delete_at_all_elements(self):
+    def test_add_at_head_after_delete_all_elements(self):
         self.populate_list(1000)
-        for i in range(0,1001):
-            self.list.deleteAtIndex(0)
         for i in range(1,1001):
-            self.list.addAtHead(i)
-            self.assertEqual(i,self.list.get(0))
             self.list.deleteAtIndex(0)
+        self.list.addAtHead(46)
+        self.assertEqual(46,self.list.get(0))
 
 if __name__=="__main__":
     unittest.main()
